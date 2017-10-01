2a8c057760fd17305a64f137226584fb35673562
Mike Causer <mcauser@gmail.com>
2017-10-02 10:25:48 +1100

    Tabs to spaces

--- max7219.py
+++ max7219.py
@@ -12,64 +12,64 @@ _SHUTDOWN = const(12)
 _DISPLAYTEST = const(15)
 
 class Matrix8x8:
-	def __init__(self, spi, cs, num):
-		"""
-		Driver for cascading MAX7219 8x8 LED matrices.
+    def __init__(self, spi, cs, num):
+        """
+        Driver for cascading MAX7219 8x8 LED matrices.
 
-		>>> import max7219
-		>>> from machine import Pin, SPI
-		>>> spi = SPI(1)
-		>>> display = max7219.Matrix8x8(spi, Pin('X5'), 4)
-		>>> display.text('1234',0,0,1)
-		>>> display.show()
+        >>> import max7219
+        >>> from machine import Pin, SPI
+        >>> spi = SPI(1)
+        >>> display = max7219.Matrix8x8(spi, Pin('X5'), 4)
+        >>> display.text('1234',0,0,1)
+        >>> display.show()
 
-		"""
-		self.spi = spi
-		self.cs = cs
-		self.cs.init(cs.OUT, True)
-		self.buffer = bytearray(8 * num)
-		self.num = num
-		fb = framebuf.FrameBuffer(self.buffer, 8 * num, 8, framebuf.MONO_HLSB)
-		self.framebuf = fb
-		# Provide methods for accessing FrameBuffer graphics primitives. This is a workround
-		# because inheritance from a native class is currently unsupported.
-		# http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
-		self.fill = fb.fill  # (col)
-		self.pixel = fb.pixel # (x, y[, c])
-		self.hline = fb.hline  # (x, y, w, col)
-		self.vline = fb.vline  # (x, y, h, col)
-		self.line = fb.line  # (x1, y1, x2, y2, col)
-		self.rect = fb.rect  # (x, y, w, h, col)
-		self.fill_rect = fb.fill_rect  # (x, y, w, h, col)
-		self.text = fb.text  # (string, x, y, col=1)
-		self.scroll = fb.scroll  # (dx, dy)
-		self.blit = fb.blit  # (fbuf, x, y[, key])
-		self.init()
+        """
+        self.spi = spi
+        self.cs = cs
+        self.cs.init(cs.OUT, True)
+        self.buffer = bytearray(8 * num)
+        self.num = num
+        fb = framebuf.FrameBuffer(self.buffer, 8 * num, 8, framebuf.MONO_HLSB)
+        self.framebuf = fb
+        # Provide methods for accessing FrameBuffer graphics primitives. This is a workround
+        # because inheritance from a native class is currently unsupported.
+        # http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
+        self.fill = fb.fill  # (col)
+        self.pixel = fb.pixel # (x, y[, c])
+        self.hline = fb.hline  # (x, y, w, col)
+        self.vline = fb.vline  # (x, y, h, col)
+        self.line = fb.line  # (x1, y1, x2, y2, col)
+        self.rect = fb.rect  # (x, y, w, h, col)
+        self.fill_rect = fb.fill_rect  # (x, y, w, h, col)
+        self.text = fb.text  # (string, x, y, col=1)
+        self.scroll = fb.scroll  # (dx, dy)
+        self.blit = fb.blit  # (fbuf, x, y[, key])
+        self.init()
 
-	def _write(self, command, data):
-		self.cs(0)
-		for m in range(self.num):
-			self.spi.write(bytearray([command, data]))
-		self.cs(1)
+    def _write(self, command, data):
+        self.cs(0)
+        for m in range(self.num):
+            self.spi.write(bytearray([command, data]))
+        self.cs(1)
 
-	def init(self):
-		for command, data in (
-			(_SHUTDOWN, 0),
-			(_DISPLAYTEST, 0),
-			(_SCANLIMIT, 7),
-			(_DECODEMODE, 0),
-			(_SHUTDOWN, 1),
-		):
-			self._write(command, data)
+    def init(self):
+        for command, data in (
+            (_SHUTDOWN, 0),
+            (_DISPLAYTEST, 0),
+            (_SCANLIMIT, 7),
+            (_DECODEMODE, 0),
+            (_SHUTDOWN, 1),
+        ):
+            self._write(command, data)
 
-	def brightness(self, value):
-		if not 0 <= value <= 15:
-			raise ValueError("Brightness out of range")
-		self._write(_INTENSITY, value)
+    def brightness(self, value):
+        if not 0 <= value <= 15:
+            raise ValueError("Brightness out of range")
+        self._write(_INTENSITY, value)
 
-	def show(self):
-		for y in range(8):
-			self.cs(0)
-			for m in range(self.num):
-				self.spi.write(bytearray([_DIGIT0 + y, self.buffer[(y * self.num) + m]]))
-			self.cs(1)
+    def show(self):
+        for y in range(8):
+            self.cs(0)
+            for m in range(self.num):
+                self.spi.write(bytearray([_DIGIT0 + y, self.buffer[(y * self.num) + m]]))
+            self.cs(1)
